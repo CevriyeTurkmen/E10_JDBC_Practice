39b2cac0b755ca5f67477af2fb630a9a6bd262b4
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-03-23 11:20:20 +0000

    Changes

--- src/test/java/jdbctests/jdbc_examples.java
+++ src/test/java/jdbctests/jdbc_examples.java
@@ -1,5 +1,6 @@
 package jdbctests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.*;
@@ -12,6 +13,7 @@ public class jdbc_examples {
     String dbUsername="hr";
     String dbPassword="hr";
 
+    @DisplayName("Result.Set methods")
     @Test
     public void test1() throws SQLException {
 
@@ -31,11 +33,67 @@ public class jdbc_examples {
 
     }
 
-    @Test
-    public void test2(){
+@Test
+    public void test2() throws SQLException {
+        Connection connection= DriverManager.getConnection(dbUrl,dbPassword,dbUsername);
+        Statement statement=connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet=statement.executeQuery("SELECT * FROM departments");
 
 
+        //How to find how many rows we have for the query
+    resultSet.last();
+    System.out.println(resultSet.getRow());
+
+    //print all second column information
+
+    resultSet.beforeFirst();
+    while (resultSet.next()){
+        System.out.println(resultSet.getString(2));
     }
 
 
 }
+
+
+    @Test
+    public void test3() throws SQLException {
+        Connection connection= DriverManager.getConnection(dbUrl,dbPassword,dbUsername);
+        Statement statement=connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet=statement.executeQuery("SELECT * FROM departments");
+
+//get the database related data inside dbMetadata object
+
+        DatabaseMetaData dbMetadata= connection.getMetaData();
+        System.out.println("dbMetadata.getUserName() = " + dbMetadata.getUserName());
+        System.out.println("dbMetadata.getDatabaseProductName() = " + dbMetadata.getDatabaseProductName());
+        System.out.println("dbMetadata.getDatabaseProductVersion() = " + dbMetadata.getDatabaseProductVersion());
+        System.out.println("dbMetadata.getDriverName() = " + dbMetadata.getDriverName());
+        System.out.println("dbMetadata.getDriverVersion() = " + dbMetadata.getDriverVersion());
+
+
+
+ResultSetMetaData rsMetaData= resultSet.getMetaData();
+
+//how many colums we have?
+
+        int colCount=rsMetaData.getColumnCount();
+
+        System.out.println("rsMetaData.getColumnCount() = " + colCount);
+
+// get the colums names
+
+        System.out.println("rsMetaData.getColumnName(2) = " + rsMetaData.getColumnName(2));
+
+        for (int i = 1; i <= colCount; i++) {
+            System.out.println(rsMetaData.getColumnName(i));
+        }
+
+        }
+
+
+//close connection
+
+    }
+
+
+
